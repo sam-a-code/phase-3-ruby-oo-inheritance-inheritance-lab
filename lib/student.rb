56f0510746d4bc9bc45d17a99498c40a861caed6
Sam Avery <sam.avery1@gmail.com>
2022-10-12 15:13:21 -0500

    commit tree 25955ecd7ce636709106a1fe2b8d62e47b68b0e5

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
 
+    def initialize
+        @knowledge = []
+    end 
+
+    def learn(learning)
+        @knowledge << learning
+    end
 end
